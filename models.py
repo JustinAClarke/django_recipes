7dd7be06fc75b229a2878b75edf1acc5fcea418e
Justin Fuhrmeister-Clarke <justin@fuhrmeister-clarke.com>
2017-11-22 21:32:59 +1100

    Modified ingredients, and changed base template

--- models.py
+++ models.py
@@ -26,17 +26,6 @@ class Category(models.Model):
     def __str__(self):
         return self.Title
 
-    
-class Ingredient(models.Model):
-    Title = models.CharField(max_length=250)
-    Unit = models.CharField(max_length=250)
-#    Recipe = models.ForeignKey(Recipe)
-    Quantity = models.FloatField()
-    
-    def __str__(self):
-        return self.Title
-
-
 class Recipe(models.Model):
     Title = models.CharField(max_length=250)
     Category = models.ForeignKey(Category)
@@ -45,12 +34,21 @@ class Recipe(models.Model):
     Cook_Time_hour = models.FloatField(default=0)
     Cook_Time_min = models.FloatField(default=0)
     Serves = models.FloatField(default=0)
-    Ingredients = models.ManyToManyField(Ingredient)
+    #Ingredients = models.ManyToManyField(Ingredient)
     Method = models.TextField()
     Notes = models.TextField(null=True)
     Image = models.CharField(max_length=250,null=True)
     Deleted = models.NullBooleanField()
     
+    
+    def __str__(self):
+        return self.Title
+
+class Ingredient(models.Model):
+    Title = models.CharField(max_length=250)
+    Unit = models.CharField(max_length=250)
+    Recipe = models.ForeignKey(Recipe)
+    Quantity = models.FloatField()
     
     def __str__(self):
         return self.Title
